92df56edc2d521064178f61ba33f2243afe9d02d
houseteru202 <houseteru202@gmail.com>
2025-02-06 23:17:43 +0900

    3章完了コミット

---
+++ app/_components/newsList/index.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./index.module.css";
+import { News } from "@/app/_libs/microcms";
+import Category from "../Category";
+import Date from "../Date";
+
+type Props = {
+  news: News[];
+};
+
+export default function NewsList({ news }: Props) {
+  if (news.length === 0) {
+    return <p>記事がありません。</p>;
+  }
+  return (
+    <ul>
+      {news.map((article) => (
+        <li key={article.id} className={styles.list}>
+          <Link href={`/news/${article.id}`} className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/no-image.png"
+              alt="No Image"
+              width={1200}
+              height={630}
+            />
+            <dl className={styles.content}>
+              <dt className={styles.title}>{article.title}</dt>
+              <dd className={styles.meta}>
+                {/* <span className="style.tag">{article.category.name}</span> */}
+                {/*<span className={styles.date}>
+                  <Image
+                    src="/clock.svg"
+                    alt=""
+                    width={16}
+                    height={16}
+                    priority
+                  />
+                  {article.publishedAt}
+                </span>*/}
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
+              </dd>
+            </dl>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
