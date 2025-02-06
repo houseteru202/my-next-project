80b6b7e31e7e2ed26095332bef8bd8bbbcea66a2
houseteru202 <houseteru202@gmail.com>
2025-02-06 22:21:54 +0900

    3回目のコミット

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
+import ButtonLink from "@/app/_components/ButtonLink";
+
 type News = {
   id: string;
   title: string;
@@ -96,6 +98,9 @@ export default function Home() {
             </li>
           ))}
         </ul>
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっといる</ButtonLink>
+        </div>
       </section>
     </>
   );
