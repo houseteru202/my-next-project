f4585b8657388d82e99656dcc71448bad57bbd97
houseteru202 <houseteru202@gmail.com>
2025-02-23 00:24:30 +0900

    4章反映 refactaling

--- app/_components/newsList/index.tsx
+++ app/_components/newsList/index.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
-import styles from './index.module.css';
-import Category from '../Category';
-import Date from '../Date';
-import { News } from '@/app/_libs/microcms';
+import styles from "./index.module.css";
+import Category from "../Category";
+import Date from "../Date";
+import { News } from "@/app/_libs/microcms";
 
 type Props = {
   news: News[];
@@ -12,7 +12,7 @@ type Props = {
 
 export default function NewsList({ news }: Props) {
   if (news.length === 0) {
-    return <p>記事がありません。</p>;
+    return <p>tst記事がありません。</p>;
   }
   return (
     <ul>
