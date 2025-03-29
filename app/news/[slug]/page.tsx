b63c4f4189463eccbbd6c0d5aff3a9169dd005cc
houseteru202 <houseteru202@gmail.com>
2025-03-29 22:38:39 +0900

    ニュースページ更新修正

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -15,7 +15,7 @@ type Props = {
   }>;
 };
 
-export const revalidata = 60;
+export const revalidate = 60;
 /* export default async function Page({ params }: Props) {
   const data = await getNewsDetail(params.slug).catch(notFound); */
 
