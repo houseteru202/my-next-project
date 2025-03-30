8c10801291ea25cb054e2a9e299a943383d104d7
houseteru202 <houseteru202@gmail.com>
2025-03-30 11:14:00 +0900

    information

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ export default async function Home() {
               height={4000}
             />
             <div>
-              <h2 className={styles.sectionTitleEn}>Event Infomation</h2>
+              <h2 className={styles.sectionTitleEn}>Event Information</h2>
               <p className={styles.sectionDescription}>
                 BLACK
                 MARKETは子供も大人も楽しめるネオ縁日。老舗銭湯たつの湯の駐車場で石神井界隈の個性豊かな店主達による年に一度のお祭りが今年も開催。たつの湯のお湯並みに熱い1日を体感せよ！
